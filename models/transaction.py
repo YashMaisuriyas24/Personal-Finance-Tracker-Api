2bc00b59549baa172a5a797de8e50cba08cd1781
YashMaisuriyas24 <yashmaisuriyas04@gmail.com>
2026-02-19 14:26:14 +0530

    adding the transaction.py file

--- models/transaction.py
+++ models/transaction.py
@@ -18,12 +18,14 @@ class TransactionCreate(BaseModel):
             raise ValueError("Amount must be greater than 0")
         return i
 
+
     @field_validator("type")
     @classmethod
     def validate_type(cls, i):
         if i not in ["income", "expense"]:
             raise ValueError("Type must be income or expense")
         return i
+
 
     @field_validator("category")
     @classmethod
@@ -32,12 +34,14 @@ class TransactionCreate(BaseModel):
             raise ValueError("Category cannot be empty")
         return i.lower()
 
+
     @field_validator("date")
     @classmethod
     def date_not_future(cls, i):
         if i > datetime.now():
             raise ValueError("Date cannot be future")
         return i
+
 
     @field_validator("tags")
     @classmethod
